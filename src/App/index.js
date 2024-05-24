4d67bbfe9e2fdc596d65777ed7e47b28b0c5cc78
angel melendez <angelmelen88@gmail.com>
2024-05-24 17:20:07 -0600

    feat: class composicion

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,55 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+import React from "react";
+// import { TodoProvider } from "../TodoContext";
+// import { AppUI } from "./AppUI";
+// import { TodoSearch } from "../TodoSearch";
+// import { TodoItem } from "../TodoItem";
 
 function App() {
+  const [state, setState] = React.useState('animo');
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </React.Fragment>
   );
 }
+
+function TodoHeader({ children }) {
+  return (
+    <header>{children}</header>
+    // <React.Fragment>
+    //   <TodoCounter />
+    //   <TodoSearch />
+    // </React.Fragment>
+  );
+}
+
+function TodoList({ children }) {
+  return <section>{children}</section>;
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoCounter</p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItems { state }</p>;
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
