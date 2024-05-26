7e3e1ce81b09fe4e4846152f4cd70ca415988fc0
angel melendez <angelmelen88@gmail.com>
2024-05-25 22:03:55 -0600

    feat: style and reto

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,12 +1,24 @@
 import React from "react";
 import { withStorageListener } from "./withStorageListener";
+import './ChangeAlert.css'
 
-export const ChangeAlert = ({show, toggleShow}) => {
+export const ChangeAlert = ({ show, toggleShow }) => {
   if (show) {
     return (
-      <div>
-        <p>hubo cambios </p>
-        <button onClick={() => toggleShow(false)}>volver a cargar</button>
+      <div className="ChangeAlert-bg">
+        <div className="ChangeAlert-container">
+          <p>
+            Parece que cambiaste tus TODOs en otra pestaña o ventana del
+            navegador.
+          </p>
+          <p>¿Quieres sincronizar tus TODOs?</p>
+          <button
+            className="TodoForm-button TodoForm-button--add"
+            onClick={() => toggleShow(false)}
+          >
+            volver a cargar
+          </button>
+        </div>
       </div>
     );
   } else {
